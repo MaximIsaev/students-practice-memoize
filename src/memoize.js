f977f7a8da154cf829359ff839438bfb4edaeeff
Isaev Maxim <Maxim_Isaev1@epam.com>
2015-10-02 15:08:10 +0300

    Added content

--- src/memoize.js
+++ src/memoize.js
@@ -1,5 +1,26 @@
 export { memoize };
 
 function memoize(func) {
-    return func; // TODO
+ var CACHE = {};
+  var mainFunc = function() {
+   var buffer = " ";
+
+   for(var i = 0; i < arguments.length; i++) {
+    buffer += (typeof arguments[i]) +":"+ arguments[i] + " ";
+	  } 
+   console.log('Parameters:' + buffer);
+
+   if(buffer in CACHE) {
+    console.log("Location: from Cache" );
+    return CACHE[buffer];
+   } else {
+    console.log("Location: not cache");
+    CACHE[buffer] = func.apply(this,arguments);
+    return CACHE[buffer]; 
+   }
+  };
+  return mainFunc;
 }
+
+
+
